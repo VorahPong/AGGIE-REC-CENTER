997e858a9a7dcac4bb8c3f9cb7c70545e7de5ca0
VorahPong <vorahpongmean@gmail.com>
2024-08-27 23:09:49 -0500

    Added Register page
    Modified index.html
        - the "Guest" button now turn into a "Register" button.
    
    Create a folder containing files for RegisterPage.

--- javascript.js
+++ javascript.js
@@ -1,3 +1,15 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://playground-db759-default-rtdb.firebaseio.com/"
+}
+
+const app = initializeApp(appSettings);
+const database = getDatabase(app);
+const accountInDB = ref(database, "accounts");
+
+
 // test account
 const admin_id = 'admin';
 const admin_password = '999';
