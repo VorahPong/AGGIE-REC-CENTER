cc21ba4a1ffb8127e0d60fb546362ac71162a509
VorahPong <vorahpongmean@gmail.com>
2024-08-27 23:22:15 -0500

    Added buttons & text input on Register Page
    However, the page is not fully function yet.

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
+// connect to database
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+import { getDatabase, ref } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://playground-db759-default-rtdb.firebaseio.com/"
@@ -8,6 +9,7 @@ const appSettings = {
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
 const accountInDB = ref(database, "accounts");
+//
 
 
 // test account
@@ -15,9 +17,10 @@ const admin_id = 'admin';
 const admin_password = '999';
 //
 
+// Get user inputs for login
 const user_id = document.querySelector('#user_id');
 const user_password = document.querySelector('#user_password');
-
+//
 
 // when click login button
 const login_btn = document.querySelector('#login_btn');
@@ -29,4 +32,11 @@ login_btn.addEventListener('click', function(){
     else{
         alert('account does not exist');
     }
+});
+//
+
+// when register button is click
+const register_btn = document.querySelector('#register_btn');
+register_btn.addEventListener('click', function() {
+    window.location.href = './RegisterPage/register.html';
 });
