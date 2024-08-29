50f4c1debb066f9e68d8fe38e999c719772b15bc
VorahPong <vorahpongmean@gmail.com>
2024-08-29 11:28:06 -0500

    Login and Register are now working

--- RegisterPage/register.js
+++ RegisterPage/register.js
@@ -1,28 +1,97 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
+import { getFirestore, setDoc, doc } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
 
-const appSettings = {
-    databaseURL: "https://playground-db759-default-rtdb.firebaseio.com/"
-}
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBdMd1YfF6bmNUVsBWnlmxn7kFCKviGHrY",
+  authDomain: "playground-db759.firebaseapp.com",
+  databaseURL: "https://playground-db759-default-rtdb.firebaseio.com",
+  projectId: "playground-db759",
+  storageBucket: "playground-db759.appspot.com",
+  messagingSenderId: "686238141979",
+  appId: "1:686238141979:web:f41c5c10271c38128572be"
+};
 
-const app = initializeApp(appSettings);
-const database = getDatabase(app);
-const accountInDB = ref(database, "accounts");
-//
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+ 
 
 // Get user inputs for register
-const user_id = document.querySelector('#user_id');
-const user_password = document.querySelector('#user_password');
-const user_password_confirm = document.querySelector('#user_password_confirm');
+const user_id = document.querySelector('#user_id').value;
+const user_firstName = document.querySelector('#user_firstName').value;
+const user_lastName = document.querySelector('#user_lastName').value;
+const user_password_confirm = document.querySelector('#user_password_confirm').value;
 //
 
 const confirm_btn = document.querySelector('#confirm_btn');
-confirm_btn.addEventListener('click', function() {
-    if(user_password.value === user_password_confirm.value) {
-        alert("Account has been created");
-        push(accountInDB, `user: ${user_id.value}, password: ${user_password.value}`);
+confirm_btn.addEventListener('click', (event) => {
+    event.preventDefault();
+
+    const email = document.querySelector('#user_email').value;
+    const password = document.querySelector('#user_password').value;
+
+    //alert("good");
+    // if(validate_email(user_email) === false) {
+    //     alert('Email is not valid!!');
+    //     return;
+    // }
+    // if(validate_field(user_firstName) === false || validate_field(user_lastName) === false || validate_field(user_id) == false) {
+    //     alert('Missing information');
+    //     return;
+    // }
+    // if(password !== user_password_confirm) {
+    //     alert('Password does not match!');
+    //     return;
+    // }
+    
+
+    // if everything are good, move on with auth
+    createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed up 
+        const user = userCredential.user;
+        alert("Account created");
+        window.location.href = "../index.html";
+        // ...
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorMessage);
+        // ..
+    });
+});
+
+//
+function validate_email(user_email) {
+    if(/^[^@]+@\w+(\.\w+)+\w$/.test(user_email) === true) {
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+function validate_password(user_password) {
+    // firebase only accepts lengths greater than 6
+    if(user_password < 6) {
+        return false;
     }
     else {
-        alert("Your password does not match");
+        return true;
     }
-});
+}
+
+function validate_field(field) {
+    if(field === null) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
--- javascript.js
+++ javascript.js
@@ -1,37 +1,52 @@
-// connect to database
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import { getDatabase, ref } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 
-const appSettings = {
-    databaseURL: "https://playground-db759-default-rtdb.firebaseio.com/"
-}
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBdMd1YfF6bmNUVsBWnlmxn7kFCKviGHrY",
+  authDomain: "playground-db759.firebaseapp.com",
+  databaseURL: "https://playground-db759-default-rtdb.firebaseio.com",
+  projectId: "playground-db759",
+  storageBucket: "playground-db759.appspot.com",
+  messagingSenderId: "686238141979",
+  appId: "1:686238141979:web:f41c5c10271c38128572be"
+};
 
-const app = initializeApp(appSettings);
-const database = getDatabase(app);
-const accountInDB = ref(database, "accounts");
-//
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 
 // test account
-const admin_id = 'admin';
-const admin_password = '999';
+// const admin_id = 'admin';
+// const admin_password = '999';
 //
 
-// Get user inputs for login
-const user_id = document.querySelector('#user_id');
-const user_password = document.querySelector('#user_password');
-//
+
 
 // when click login button
 const login_btn = document.querySelector('#login_btn');
 login_btn.addEventListener('click', function(){
-    if(user_id.value === admin_id && user_password.value === admin_password){
-        alert('login successful');
+    // Get user inputs for login
+    const email = document.querySelector('#user_email').value;
+    const password = document.querySelector('#user_password').value;
+    //
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+        // Signed in 
+        const user = userCredential.user;
+        alert('Login Successful');
         window.location.href = './HomePage/home.html';
-    }
-    else{
-        alert('account does not exist');
-    }
+        // ...
+    })
+    .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        alert(errorMessage);
+    });
 });
 //
 
